24464a63e0e1cb7785cf4ceaae9e01e441b97935
Lin Htet Lwin <253437n@mymail.nyp.edu.sg>
2025-08-04 14:03:33 +0800

    Add files via upload

---
+++ website/js/home_card.js
@@ -0,0 +1,25 @@
+console.log("home card running")
+fetch('../json/activities.json')
+  .then(response => response.json())
+  .then(activities => {
+    const row = document.getElementById('activity-row');
+    activities.forEach(activity => {
+      const card = document.createElement('div');
+      card.className = 'card-parent';
+      card.innerHTML = `
+        <a href="${activity.link}">
+          <div class="card">
+            <div class="card-image">
+              <img src="${activity.image}" class="card-img-top" alt="${activity.title}" aria-hidden="true">
+            </div>
+            <div class="card-body d-flex row">
+              <h1>${activity.title}</h1>
+              <h2>${activity.date}</h2>
+              <p class="card-text">${activity.description}</p>
+            </div>
+          </div>
+        </a>
+      `;
+      row.appendChild(card);
+    });
+  });
